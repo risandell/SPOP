5f8d28ea6a46bad47f522fa5ff3f23e9e96df810
risandell <risandell@wp.pl>
2013-12-23 14:46:32 +0100

    Ogólnie co na razie działa: - dodawanie nowych zadań do listy zaplanowanych - usuwanie zadań po nazwie lub wszystkich zadań (zaplanowanych/zrealizowanych) - wyświetlanie wszystkich zadań na liście (zaplanowanych/zrealizowanych) - jest opcja dodania zadania jako zrealizowanego (trzeba dorobić usuwanie zadania jednorazowego lub zmiana daty przy innej powtarzalności zadania) Zostało jeszcze do zrobienie: * zapisywanie do pliku / odczytywanie z pliku * przegladanie listy zaplanowanych w dniu dzisiejszym (w tym zalegle) * podanie "dzisiejszej" daty i godziny <- nie do końca wiem o co mu chodzi z tym

--- Model.hs
+++ Model.hs
@@ -1,6 +1,6 @@
 module Model where
 
--- import Char
+-- biblioteki
 import Data.Char
 import Data.Time hiding (Day)
 
@@ -20,61 +20,71 @@ data Zadanie = Zadanie Nazwa DataZadania Godzina Powtarzalnosc deriving (Eq, Sho
 class ListaZadan lz where
     empty :: lz -- zwraca pusta liste
     insert :: Zadanie -> lz -> lz -- dodaje zadanie do listy
-    --delete :: Zadanie -> lz -> lz -- ususwa element z listy
-    deleteAll :: lz -> lz
-    --show :: lz -> [String] -- wyswietla kolejne elementy listy
--- lista zaplanowanych zadañ
-data ListaZaplanowanych = LZap [Zadanie] deriving Show
-instance ListaZadan ListaZaplanowanych where
-    empty = LZap []
-    insert z (LZap lzap) = LZap (lzap ++ [z])
-    deleteAll (LZap lzap) = empty
-    --delete z empty = empty
-    --delete z (LZap [x]) | z == x = empty
--- lista zrealizowanych zadañ
-data ListaZrealizowanych = LZre [Zadanie] deriving Show
-instance ListaZadan ListaZrealizowanych where
-    empty = LZre []
-    insert z (LZre lzre) = LZre (lzre ++ [z])
-    deleteAll (LZre lzre) = empty
+    insertAll :: [Zadanie] -> lz -> lz -- dodaje liste zadan
+    delete :: String -> lz -> lz -- ususwa element z listy
+    deleteAll :: lz -> lz  -- usuwa wszystkie elementy z listy
+    showAll :: lz -> String -- wyswietla wszystkie elementy na liscie
+    findByName :: String -> lz -> [Zadanie] -- znajduje wszystkie zadania o podanej nazwie
+data LZad = LZ [Zadanie] deriving (Show, Eq)
+-- instancja listy zadañ
+instance ListaZadan LZad where
+    empty = LZ []
+    insert z (LZ lz) = LZ (lz ++ [z])
+    insertAll [] (LZ lz) = LZ lz
+    insertAll (z) (LZ lz) = LZ newLz where
+        newLz = lz ++ z
+    delete nazwa (LZ lz) = LZ newLz where
+        newLz = concat (map (\x -> if pobierzNazwe x == nazwa then [] else [x]) lz)
+    deleteAll (LZ lz) = empty
+    showAll (LZ lz) = "*********\n" ++ wynik ++ "\n*********\n" where
+        wynik = if (LZ lz) == empty then "Brak zadan na liscie"
+            else "Zadania: " ++ el
+        el = concat (map (\x -> "\n- " ++ pobierzZadanie x) lz)
+    findByName nazwa (LZ lz) = filter (\x -> pobierzNazwe x == nazwa) lz
 -- ******************
 -- Funkcje pomocnicze
 -- ******************
+-- funkcja pobieraj¹ca nazwê zadania
+pobierzNazwe :: Zadanie -> String
+pobierzNazwe (Zadanie nazwa dataZadania godzina powtarzalnosc) = nazwa
+-- funckja pobieraj¹ca zadanie do wyswietlania
+pobierzZadanie :: Zadanie -> String
+pobierzZadanie (Zadanie nazwa (DataZadania dzien miesiac rok) godzina powtarzalnosc) = nazwa ++ ", " ++ (dzien2string dzien) ++ " " ++ (miesiac2string miesiac) ++ " " ++ (show rok) ++ ", " ++ (show godzina) ++ ", " ++ (powtarzalnosc2string powtarzalnosc) 
 -- sprawdza czy dany string sk³ada siê tylko z liczb/liter
 czyString :: String -> Bool
 czyString [] = False
 czyString [x] | isAlphaNum x = True
-              | otherwise = False
+    | otherwise = False
 czyString (x:xs) | isAlphaNum x = czyString xs
-                 | otherwise = False
+    | otherwise = False
 -- sprawdza czy poprawny dzien
 czyDzien :: String -> Bool
 czyDzien [] = False
 czyDzien xs | xs == "Pon" || xs == "Wt" || xs == "Sr" || xs == "Czw" || xs == "Pt" || xs == "Sob" || xs == "Nie" = True
-            | otherwise = False
+    | otherwise = False
 -- sprawdza czy poprawny miesiac
 czyMiesiac :: String -> Bool
 czyMiesiac [] = False
 czyMiesiac xs | xs == "Sty" || xs == "Lut" || xs == "Mar" || xs == "Kwi" || xs == "Maj" || xs == "Cze" || xs == "Lip" || xs == "Sie" || xs == "Wrz" || xs == "Paz" || xs == "Lis" || xs == "Gru" = True
-              | otherwise = False
+    | otherwise = False
 -- sprawdza czy liczba
 czyLiczba [] = False
 czyLiczba [x] | isDigit x = True
-              | otherwise = False
+    | otherwise = False
 czyLiczba (x:xs) | isDigit x = czyLiczba xs
-                 | otherwise = False
+    | otherwise = False
 -- sprawdza czy poprawna godzina (hh:mm)
 czyGodzina :: String -> Bool
 czyGodzina [] = False
 czyGodzina (a:b:c:d:e:f) | a >= '0' && a <= '1' && (isDigit b) && c == ':' && d >= '0' && d <= '5' && (isDigit e) && f ==[] = True
-                         | a == '2' && b >= '0' && b <= '3' && c == ':' && d >= '0' && d <= '5' && (isDigit e) && f == [] = True
-						 | otherwise = False
+    | a == '2' && b >= '0' && b <= '3' && c == ':' && d >= '0' && d <= '5' && (isDigit e) && f == [] = True
+    | otherwise = False
 czyGodzina xs | (length xs) >= 6 || (length xs) <= 4 = False
 -- sprawdza czy poprawny okres wystêpowania zdarzenia
 czyPowtarzalnosc :: String -> Bool
 czyPowtarzalnosc [] = False
 czyPowtarzalnosc xs | xs == "jednorazowe" || xs == "co dzien" || xs == "co tydzien" || xs == "co miesiac" || xs == "co rok" = True
-                    | otherwise = False
+    | otherwise = False
 -- zamiana String w Dzien
 string2Dzien :: String -> Dzien
 string2Dzien [] = error "Pusty string"
@@ -87,6 +97,31 @@ string2Dzien xs = case xs of
     "Sob" -> Sob
     "Nie" -> Nie
     otherwise -> error "Niepoprawny string"
+-- Zamiana Dzien w string
+dzien2string :: Dzien -> String
+dzien2string d = case d of
+    Pon -> "poniedzialek"
+    Wt -> "wtorek"
+    Sr -> "sroda"
+    Czw -> "czwartek"
+    Pt -> "piatek"
+    Sob -> "sobota"
+    Nie -> "niedziela"
+-- zamiana Miesiac w String
+miesiac2string :: Miesiac -> String
+miesiac2string m = case m of 
+    Sty -> "styczen"
+    Lut -> "luty"
+    Mar -> "marzec"
+    Kwi -> "kwiecien"
+    Maj -> "maj"
+    Cze -> "czerwiec"
+    Lip -> "lipiec"
+    Sie -> "sierpien"
+    Wrz -> "wrzesien"
+    Paz -> "pazdziernik"
+    Lis -> "listopad"
+    Gru -> "grudzien"
 -- zamiana String w Miesiac
 string2Miesiac :: String -> Miesiac
 string2Miesiac [] = error "Pusty string"
@@ -108,24 +143,31 @@ string2Miesiac xs = case xs of
 string2Powtarzalnosc :: String -> Powtarzalnosc
 string2Powtarzalnosc [] = error "Pusty string"
 string2Powtarzalnosc xs | xs == "jednorazowe" = Jednorazowe
-                  | xs == "co dzien" = Co_dzien
-                  | xs == "co tydzien" = Co_tydzien
-                  | xs == "co miesiac" = Co_miesiac
-                  | xs == "co rok" = Co_rok
-                  | otherwise = error "Niepoprawny string"
+    | xs == "co dzien" = Co_dzien
+    | xs == "co tydzien" = Co_tydzien
+    | xs == "co miesiac" = Co_miesiac
+    | xs == "co rok" = Co_rok
+    | otherwise = error "Niepoprawny string"
+-- zamiana Powtarzalnosc w String
+powtarzalnosc2string :: Powtarzalnosc -> String
+powtarzalnosc2string p = case p of
+    Jednorazowe -> "jednorazowe"
+    Co_dzien -> "co dzien"
+    Co_tydzien -> "co tydzien"
+    Co_miesiac -> "co miesiac"
+    Co_rok -> "co rok"
 -- zamiana String w Int
 string2int :: String -> Int
 string2int [] = 0
 string2int (x:xs) = if(checkMinus x) then -1 * change2int(xs)
-                    else change2int(x:xs)
-
+    else change2int(x:xs)
 checkMinus :: Char -> Bool
 checkMinus a = if(a=='-') then True
-               else False
+    else False
 change2int [] = 0
 change2int (x:xs) = if(isDigit x) then result
-                    else change2int(xs)
-					where result = dig * exp + change2int(xs)
-					      len = length(xs)
-					      exp = 10^len
-					      dig = digitToInt x
+    else change2int(xs)
+    where result = dig * exp + change2int(xs)
+          len = length(xs)
+          exp = 10^len
+          dig = digitToInt x
--- Przypomnienia.hs
+++ Przypomnienia.hs
@@ -16,7 +16,7 @@ main = do
 -- menu g³ówne
 -- przyjmuje dwa parametry (liste zaplanowanych i liste zrealizowanych)
 -- ***********
-menu (LZap lzap, LZre lzre)= do
+menu (LZ lzap, LZ lzre)= do
     putStrLn "*****Program - Przypomnienia*****"
     putStrLn "***Mozliwe operacje:"
     putStrLn "*1. Zarzadzanie lista zaplanowanych zadan"
@@ -27,42 +27,52 @@ menu (LZap lzap, LZre lzre)= do
     putStrLn "*********************************"
     option <- getLine
     case option of
-        "1" -> zarzadzanieZaplanowanymi(LZap lzap, LZre lzre)
-        "2" -> zarzadzanieZrealizowanymi(LZap lzap, LZre lzre)
-        "3" -> menuZapisOdczyt(LZap lzap, LZre lzre)
+        "1" -> zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
+        "2" -> zarzadzanieZrealizowanymi(LZ lzap, LZ lzre)
+        "3" -> menuZapisOdczyt(LZ lzap, LZ lzre)
         "4" -> putStrLn "Dzisiejsza data to:"
         "5" -> return()
         otherwise -> do
             putStrLn "Zla opcja!"
-            menu (LZap lzap, LZre lzre)
+            menu (LZ lzap, LZ lzre)
 -- ************************************
 -- menu zarz¹dzania zaplanowanych zadañ
 -- ************************************
-zarzadzanieZaplanowanymi(LZap lzap, LZre lzre) = do
+zarzadzanieZaplanowanymi(LZ lzap, LZ lzre) = do
     putStrLn ""
     putStrLn "Zarzadzanie zaplanowanych zadan"
     putStrLn "Dostepne opcje:"
     putStrLn "1. Utworz nowe zadanie do wykonania"
     putStrLn "2. Wyswietl wszystkie zaplanowane zadania"
-    putStrLn "3. Wyswietl zadania do zrealizowania w dniu dzisiejszym (w tym zalegle"
-    putStrLn "4. Wybierz zadanie jako zrealizowane"
+    putStrLn "3. Wyswietl zadania do zrealizowania w dniu dzisiejszym (w tym zalegle)"
+    putStrLn "4. Wybierz zadanie jako zrealizowane (po nazwie)"
     putStrLn "5. Usun zadanie lub wszystkie zadania"
     putStrLn "6. Powrot"
     option <- getLine
     case option of
-        "1" -> menuDodawania(LZap lzap, LZre lzre)
+        "1" -> menuDodawania(LZ lzap, LZ lzre)
         "2" -> do 
-            print (LZap lzap)
-            zarzadzanieZaplanowanymi(LZap lzap, LZre lzre)
-        "5" -> menuUsuwania(LZap lzap, LZre lzre, 0)
-        "6" -> menu(LZap lzap, LZre lzre)
+            putStrLn (showAll (LZ lzap))
+            zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
+        -- TODO: na razie opcja 4 dodaje element do listy zrealizowanych
+        -- Nale¿y dodaæ usuwanie/zmianê daty w zale¿noœci od powtarzalnoœci zadania
+        "4" -> do
+            putStrLn "Podaj nazwe"
+            nazwa <- getLine
+            if czyString nazwa == False then do
+                putStrLn "Bledny ciag znakow\nSprobuj ponownie."
+                zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
+            else do
+                zarzadzanieZaplanowanymi(LZ lzap, insertAll (findByName nazwa (LZ lzap)) (LZ lzre))
+        "5" -> menuUsuwania(LZ lzap, LZ lzre, 0)
+        "6" -> menu(LZ lzap, LZ lzre)
         otherwise -> do
             putStrLn "Zla opcja!"
-            zarzadzanieZaplanowanymi(LZap lzap, LZre lzre)
+            zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
 -- **********************
 -- menu dodawania zadania
 -- **********************
-menuDodawania(LZap lzap, LZre lzre) = do
+menuDodawania(LZ lzap, LZ lzre) = do
     putStrLn ""
     putStrLn "Wprowadz nazwe zadania"
     nazwa <- getLine
@@ -80,22 +90,22 @@ menuDodawania(LZap lzap, LZre lzre) = do
     if (czyString nazwa == False) || (czyDzien dzien == False) || (czyMiesiac miesiac == False) || (czyLiczba rok == False) || (czyGodzina godzina == False)|| (czyPowtarzalnosc okres == False) then do
         putStrLn "Wprowadzono bledne dane!"
         putStrLn "Powrot do zarzadzania zaplanowanymi zadaniami"
-        zarzadzanieZaplanowanymi(LZap lzap, LZre lzre)
+        zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
 	else do
         putStrLn "Stworzono obiekt"
         print (Zadanie nazwa (DataZadania (string2Dzien dzien) (string2Miesiac miesiac) (string2int rok)) (read(godzina ++ ":00")::TimeOfDay) (string2Powtarzalnosc okres))
-        zarzadzanieZaplanowanymi((insert (Zadanie nazwa (DataZadania (string2Dzien dzien) (string2Miesiac miesiac) (string2int rok)) (read(godzina ++ ":00")::TimeOfDay) (string2Powtarzalnosc okres)) (LZap lzap)), LZre lzre)
+        zarzadzanieZaplanowanymi((insert (Zadanie nazwa (DataZadania (string2Dzien dzien) (string2Miesiac miesiac) (string2int rok)) (read(godzina ++ ":00")::TimeOfDay) (string2Powtarzalnosc okres)) (LZ lzap)), LZ lzre)
 -- *********************
 -- menu usuwania zadañ
 -- (lista zaplanowanych, lista zrealizowanych,tryb (0/1))
 -- tryb = 0 - usuwanie zaplanowanych
 -- tryb = 1 - usuwanie zrealizowanych
 -- *********************
-menuUsuwania(LZap lzap, LZre lzre,tryb) = do
+menuUsuwania(LZ lzap, LZ lzre,tryb) = do
     putStrLn ""
     if tryb == 0 then putStrLn "Usuwanie zadan zaplanowanych"
     else putStrLn "Usuwanie zadan zrealizowanych"
-    putStrLn "1. Usun podane zadanie" -- TODO usuwanie po wybranej nazwie
+    putStrLn "1. Usun zadanie o podanej nazwie"
     putStrLn "2. Usun wszystkie zadania z listy"
     putStrLn "3. Powrot do menu zarzadzania zadaniami"
     option <- getLine
@@ -105,23 +115,23 @@ menuUsuwania(LZap lzap, LZre lzre,tryb) = do
             nazwa <- getLine
             if czyString nazwa == False then do
                 putStrLn "Bledny ciag znakow\nSprobuj ponownie."
-                menuUsuwania(LZap lzap, LZre lzre,tryb)
+                menuUsuwania(LZ lzap, LZ lzre,tryb)
             else do
-                putStrLn "Na razie nie zaimplementowane"
-                menuUsuwania(LZap lzap, LZre lzre,tryb)
+                if tryb == 0 then menuUsuwania(delete nazwa (LZ lzap), LZ lzre,tryb)
+                else menuUsuwania(LZ lzap, delete nazwa (LZ lzre),tryb)
         "2" -> do
-            if tryb == 0 then zarzadzanieZaplanowanymi(deleteAll (LZap lzap),LZre lzre)
-            else zarzadzanieZrealizowanymi(LZap lzap, deleteAll (LZre lzre))
+            if tryb == 0 then zarzadzanieZaplanowanymi(deleteAll (LZ lzap),LZ lzre)
+            else zarzadzanieZrealizowanymi(LZ lzap, deleteAll (LZ lzre))
         "3" -> do
-            if tryb == 0 then zarzadzanieZaplanowanymi(LZap lzap, LZre lzre)
-            else zarzadzanieZrealizowanymi(LZap lzap, LZre lzre)
+            if tryb == 0 then zarzadzanieZaplanowanymi(LZ lzap, LZ lzre)
+            else zarzadzanieZrealizowanymi(LZ lzap, LZ lzre)
         otherwise -> do
             putStrLn "Zla opcja!"
-            menuUsuwania(LZap lzap, LZre lzre,tryb)
+            menuUsuwania(LZ lzap, LZ lzre,tryb)
 -- *************************************
 -- menu zarz¹dzania zrealizowanych zadañ
 -- *************************************
-zarzadzanieZrealizowanymi(LZap lzap, LZre lzre) = do
+zarzadzanieZrealizowanymi(LZ lzap, LZ lzre) = do
     putStrLn ""
     putStrLn "Zarzadzanie zrealizowanych zadan"
     putStrLn "Dostepne opcje:"
@@ -131,17 +141,17 @@ zarzadzanieZrealizowanymi(LZap lzap, LZre lzre) = do
     option <- getLine
     case option of
         "1" -> do
-            print (LZre lzre)
-            zarzadzanieZrealizowanymi(LZap lzap, LZre lzre)
-        "2" -> menuUsuwania(LZap lzap, LZre lzre,1)
-        "3" -> menu(LZap lzap, LZre lzre)
+            putStrLn (showAll (LZ lzre))
+            zarzadzanieZrealizowanymi(LZ lzap, LZ lzre)
+        "2" -> menuUsuwania(LZ lzap, LZ lzre,1)
+        "3" -> menu(LZ lzap, LZ lzre)
         otherwise -> do
             putStrLn "Zla opcja!"
-            zarzadzanieZrealizowanymi(LZap lzap, LZre lzre)
+            zarzadzanieZrealizowanymi(LZ lzap, LZ lzre)
 -- *******************
 -- menu odczytu/zapisu
 -- *******************
-menuZapisOdczyt(LZap lzap, LZre lzre) = do
+menuZapisOdczyt(LZ lzap, LZ lzre) = do
     putStrLn ""
     putStrLn "Menu odczytu/zapisu zaplanowanych/zrealizowanych zadan"
     putStrLn "Dostepne opcje:"
@@ -152,7 +162,7 @@ menuZapisOdczyt(LZap lzap, LZre lzre) = do
     putStrLn "5. Powrot"
     option <- getLine
     case option of
-        "5" -> menu(LZap lzap, LZre lzre)
+        "5" -> menu(LZ lzap, LZ lzre)
         otherwise -> do
             putStrLn "Zla opcja!"
-            menuZapisOdczyt(LZap lzap, LZre lzre)
+            menuZapisOdczyt(LZ lzap, LZ lzre)
